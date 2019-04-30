ced420eedc436066678845aba0c1217bbc55e5da
Reinboo <nivalis@protonmail.com>
2019-04-30 20:44:23 +0200

    Theme fix

--- src/theme.js
+++ src/theme.js
@@ -2,12 +2,13 @@ const theme = {
   bgColor: '#434343',
   panelBodyColor: '#7d7d7d',
   buttonColor: '#63636e',
+  buttonColorPressed: 'rgba(130,130,141,0.52)',
   buttonTextColor: '#f3f3f3',
   screenTextColor: '#434343',
   screenBacklightOffColor: '#47b2b2',
   screenBacklightOnColor: '#7fffff',
 
-  fontNormal: 'Roboto Mono Normal',
+  fontNormal: 'Roboto Mono',
   fontMedium: 'Roboto Mono Medium',
 };
 
