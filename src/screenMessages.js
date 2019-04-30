2e0aaa40ac82cf40d53225fdf48d5d5eba4b5966
Reinboo <nivalis@protonmail.com>
2019-04-30 20:43:51 +0200

    Add messages

---
+++ src/screenMessages.js
@@ -0,0 +1,16 @@
+const messages = {
+  main: {
+    error: 'Error',
+    ready: 'Ready',
+    locking: 'Locking...',
+    unlocking: 'Unlocking...',
+    service: 'Service',
+    validating: 'Validating...',
+  },
+  top: {
+    locked: 'Locked',
+    unlocked: 'Unlocked',
+  },
+};
+
+export default messages;
