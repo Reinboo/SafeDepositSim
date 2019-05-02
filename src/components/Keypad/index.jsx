9934bece7d6bcd4c8505667b521fa4c6f3135f80
Reinboo <nivalis@protonmail.com>
2019-05-02 19:17:18 +0200

    Lock on 'L' key instead of timeout

--- src/components/Keypad/index.jsx
+++ src/components/Keypad/index.jsx
@@ -6,16 +6,24 @@ import Key from './Key';
 
 const keyFaces = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '*', '0', 'L'];
 
-const Keypad = ({ handleUpdate }) => (
+const Keypad = ({ handleUpdate, handleLock }) => (
   <Wrapper>
-    {keyFaces.map(value => (
-      <Key keyFace={value} handleClick={handleUpdate} key={value} />
-    ))}
+    {keyFaces.map((value) => {
+      if (value === 'L') {
+        return (
+          <Key keyFace={value} handleClick={handleLock} key={value} />
+        );
+      }
+      return (
+        <Key keyFace={value} handleClick={handleUpdate} key={value} />
+      );
+    })}
   </Wrapper>
 );
 
 Keypad.propTypes = {
   handleUpdate: PropTypes.func.isRequired,
+  handleLock: PropTypes.func.isRequired,
 };
 
 const Wrapper = styled.div`
